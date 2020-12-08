d54d96d5601500519da96957d7aac909c5f92194
JASON <1179312290@qq.com>
2020-12-08 19:36:36 +0800

    搭建好了redux项目结构

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Product from "./Containers/Product"
+import Cart from "./Containers/Cart"
+
+export default function App(props) {
+    return (
+        <fieldset>
+            <legend>App</legend>
+            <Product />
+            <br />
+            <Cart />
+        </fieldset>
+    )
+}
---
+++ src/store/Action/product.js
@@ -0,0 +1,6 @@
+import * as types from "../Types/product"
+
+const createAction = (type, payload) => ({ type, payload })
+
+
+// 关于product的action
