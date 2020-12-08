f18ed81060ceace185420b6a84e9d5b0e859bdfe
JASON <1179312290@qq.com>
2020-12-08 20:07:03 +0800

    编写了actionshetypes

--- src/store/Action/product.js
+++ src/store/Action/product.js
@@ -1,6 +1,19 @@
 import * as types from "../Types/product"
 
-const createAction = (type, payload) => ({ type, payload })
+const createAction = type => payload => ({ type, payload })
 
 
 // 关于product的action
+
+
+// 发起请求
+const getProductReq = createAction(types.FETCH_PRODUCT_REQ)
+
+// 获取购物车商品成功
+const getProductSuccess = createAction(types.FETCH_PRODUCT_SUCCESS)
+
+// 获取购物车商品失败
+const getProductFail = createAction(types.FETCH_PRODUCT_FAIL)
+
+// 加入购物车
+const addTocart = createAction(types.ADD_TOCART)
--- src/store/index.js
+++ src/store/index.js
@@ -4,4 +4,12 @@ import { composeWithDevTools } from "redux-devtools-extension"
 
 import rootReducer from "./Reducers"
 
-export default createStore(rootReducer, composeWithDevTools())
+import craeteSagaMiddleware from "redux-saga" //引入saga中间件
+
+let sagaMiddleware = craeteSagaMiddleware()
+
+import helloSaga from "./Sagas/Product"
+
+export default createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)))
+
+sagaMiddleware.run(helloSaga)
